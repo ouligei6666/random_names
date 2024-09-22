b1f2e23136e8eefb26d090bc9a8296615fc3c315
zzheve2 <1224845965@qq.com>
2024-09-22 17:36:22 +0800

    random

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_random_FILE C:/Users/zhuzh/Documents/Dev/qt/random/build/Desktop_Qt_6_7_2_MinGW_64_bit-Release/random.exe)
-set(__QT_DEPLOY_TARGET_random_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_random_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
